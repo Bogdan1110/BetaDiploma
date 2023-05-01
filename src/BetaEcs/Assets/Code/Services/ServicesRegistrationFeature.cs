51e944c5341ea1e9066b9c6161b41e240ccdc77d
rapushka <mikalajsiatkouski@gmail.com>
2023-05-01 18:08:44 +0300

    load balance resource as method

--- src/BetaEcs/Assets/Code/Services/ServicesRegistrationFeature.cs
+++ src/BetaEcs/Assets/Code/Services/ServicesRegistrationFeature.cs
@@ -9,11 +9,11 @@ namespace Beta
 			: base(nameof(ServicesRegistrationFeature))
 		{
 			Register<IInputService>(new OldInputService(), contexts.services.ReplaceInputService);
-			Register<IBalanceService>(BalanceFile, contexts.services.ReplaceBalanceService);
+			Register<IBalanceService>(LoadBalance(), contexts.services.ReplaceBalanceService);
 			Register<ITimeService>(new TimeService(), contexts.services.ReplaceTimeService);
 		}
 
-		private static Balance BalanceFile => Resources.Load<Balance>(Constants.ResourcePath.Balance);
+		private static Balance LoadBalance() => Resources.Load<Balance>(Constants.ResourcePath.Balance);
 
 		private void Register<TService>(TService service, Action<TService> replace)
 			=> Add(new RegisterServiceSystem<TService>(service, replace));
