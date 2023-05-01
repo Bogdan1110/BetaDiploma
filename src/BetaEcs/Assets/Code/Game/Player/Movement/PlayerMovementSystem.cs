a9c0f2a477173415afc067972d1e58e0df4502dc
rapushka <mikalajsiatkouski@gmail.com>
2023-05-01 17:48:59 +0300

    usage of balance service

--- src/BetaEcs/Assets/Code/Game/Player/Movement/PlayerMovementSystem.cs
+++ src/BetaEcs/Assets/Code/Game/Player/Movement/PlayerMovementSystem.cs
@@ -12,9 +12,9 @@ namespace Beta
 		public PlayerMovementSystem(Contexts contexts)
 			=> _entities = contexts.game.GetGroup(AllOf(Player, Position, NetworkIdentity));
 
-		private static Vector2 Movement => Services.Input.MovementDirection * Time.deltaTime * BalancePlayerSpeed;
+		private static Vector2 Movement => Services.Input.MovementDirection * Time.deltaTime * PlayerSpeed;
 
-		private static float BalancePlayerSpeed => 5f;
+		private static float PlayerSpeed => Services.Balance.Player.Speed;
 
 		public void Execute()
 		{
--- src/BetaEcs/Assets/Code/Services/Services.cs
+++ src/BetaEcs/Assets/Code/Services/Services.cs
@@ -4,6 +4,8 @@ namespace Beta
 	{
 		public static IInputService Input => Context.inputService.Value;
 
+		public static IBalanceService Balance => Context.balanceService.Value;
+
 		private static ServicesContext Context => Contexts.sharedInstance.services;
 	}
 }
