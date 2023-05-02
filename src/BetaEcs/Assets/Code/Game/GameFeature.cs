72d6fe1b11225078895ce65de26eb9bfd00895c5
rapushka <mikalajsiatkouski@gmail.com>
2023-05-02 15:55:03 +0300

    GameFeature splitted in methods

--- src/BetaEcs/Assets/Code/Game/GameFeature.cs
+++ src/BetaEcs/Assets/Code/Game/GameFeature.cs
@@ -5,19 +5,37 @@ namespace Beta
 		public GameFeature(Contexts contexts)
 			: base(nameof(GameFeature))
 		{
-			// Initialization
-			Add(new ServicesRegistrationFeature(contexts));
+			RegisterServices(contexts);
+			Gameplay(contexts);
+			CallEntitasGeneratedStuff(contexts);
+		}
 
+		private void Gameplay(Contexts contexts)
+		{
+			Initialize(contexts);
+			ReadInput(contexts);
+			UpdateLogic(contexts);
+		}
+
+		private void Initialize(Contexts contexts)
+		{
 			Add(new SpawnCursorSystem(contexts));
 			Add(new PlayerLookAtCursorSystem(contexts));
+		}
 
-			// Update
+		private void ReadInput(Contexts contexts)
+		{
 			Add(new PlayerMovementSystem(contexts));
 			Add(new TrackCursorPositionSystem(contexts));
-			Add(new LookAtTargetSystem(contexts));
-
-			// Entitas Generated
-			Add(new GameEventSystems(contexts));
 		}
+
+		private void UpdateLogic(Contexts contexts)
+		{
+			Add(new LookAtTargetSystem(contexts));
+		}
+
+		private void RegisterServices(Contexts contexts) => Add(new ServicesRegistrationFeature(contexts));
+
+		private void CallEntitasGeneratedStuff(Contexts contexts) => Add(new GameEventSystems(contexts));
 	}
 }
