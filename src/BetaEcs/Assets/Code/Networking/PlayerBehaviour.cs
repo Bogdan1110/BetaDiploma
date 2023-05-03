0925d483572e053bfdc225ce62f39e3f1c216b74
rapushka <mikalajsiatkouski@gmail.com>
2023-05-03 16:30:08 +0300

    OOP realization

--- src/BetaEcs/Assets/Code/Networking/PlayerBehaviour.cs
+++ src/BetaEcs/Assets/Code/Networking/PlayerBehaviour.cs
@@ -1,5 +1,6 @@
 using Mirror;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Beta
 {
@@ -9,9 +10,16 @@ namespace Beta
 		[SerializeField] private PositionView _positionView;
 		[SerializeField] private RotationView _rotationView;
 		[SerializeField] private BulletSpawner _bulletSpawner;
+		[SerializeField] private int _health;
+		[SerializeField] private int _maxHealth;
+		[SerializeField] private Slider _healthBar;
+
+		[SyncVar(hook = nameof(SyncHealth))] private int _syncHealth;
 
 		private void Start()
 		{
+			_healthBar.maxValue = _maxHealth;
+
 			var e = Contexts.sharedInstance.game.CreateEntity();
 			e.isPlayer = true;
 			e.isHittable = true;
@@ -23,5 +31,33 @@ namespace Beta
 			e.AddRotationListener(_rotationView);
 			e.AddBulletSpawner(_bulletSpawner);
 		}
+
+		private void Update()
+		{
+			_healthBar.value = _health;
+
+			if (isOwned)
+			{
+				if (Input.GetKeyDown(KeyCode.H))
+				{
+					if (isServer)
+					{
+						ChangeHealthValue(_health - 5);
+					}
+					else
+					{
+						CmdChangeHealth(_health - 5);
+					}
+				}
+			}
+		}
+
+		private void SyncHealth(int oldValue, int newValue) => _health = newValue;
+
+		[Command]
+		public void CmdChangeHealth(int newValue) => ChangeHealthValue(newValue);
+
+		[Server]
+		public void ChangeHealthValue(int newValue) => _syncHealth = newValue;
 	}
 }
